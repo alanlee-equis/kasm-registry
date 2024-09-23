487d547c0e73ead6a0296c8b88d3a8db24e6ef94
alanlee-equis <alan.lee@equis.com>
2024-09-23 12:08:02 +0800

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,11 +2,11 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'Equis Repository',
+    description: 'The official Equis store for Kasm supported workspaces.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'https://github.com/alanlee-equis/kasm-registry/',
+    contactUrl: 'https://github.com/alanlee-equis/kasm-registry/issues',
   },
   reactStrictMode: true,
   swcMinify: true,
